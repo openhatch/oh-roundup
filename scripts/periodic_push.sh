acb8f831afdfeb6ef5fdda96d05e438dbd5ef55b
People who SSH into the OpenHatch roundup <roundup@linode.openhatch.org>
2011-01-21 18:46:41 +0000

    Add a periodic push script, for a cron job

---
+++ scripts/periodic_push.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd $HOME/trackers/bugs/
+
+# Make sure that "db" is not part of the repository
+if git ls-files | grep -q ^db; then
+   echo "Eek, the db is in the repository."
+   exit 1
+fi
+
+# Okay, it should be safe. Push.
+git push --quiet origin HEAD:master
